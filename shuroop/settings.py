0d539dd337a7ee8826c9f8370e4c96cccfc36bc8
ljy2855 <ljy2855@naver.com>
2022-08-04 11:56:29 +0900

    feat: Add custom user models

--- shuroop/settings.py
+++ shuroop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'rentals',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -117,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from socket import RDS_CMSG_RDMA_UPDATE
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
+class User(models.Model):
+    user_id = models.OneToOneField(User,on_delete=models.CASCADE,related_name="user") #장고 내장 User 사용
+    is_renting = models.BooleanField(default=False)
+    left_time = models.DurationField(default=datetime.timedelta())
+    ## left_money -> 이건 left_time만 해도 충분할듯?
 # Create your models here.
