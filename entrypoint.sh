85a8f30160d2e72687b3f661c9314f708a1e1003
Ubuntu <ubuntu@ip-172-31-43-176.ap-northeast-2.compute.internal>
2022-09-30 11:39:49 +0000

    fix: fix release bd remove

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,9 @@ then
 
     echo "PostgreSQL started"
 fi
-
-python manage.py flush --no-input
-python manage.py migrate
+python manage.py makemigrations
+# python manage.py flush --no-input
+python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
 exec "$@"
