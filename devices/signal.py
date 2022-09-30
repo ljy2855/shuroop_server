9f2e0ba81cedbd2bfd9100d6473c4dcc3eecc454
ljy2855 <ljy2855@naver.com>
2022-09-30 20:24:11 +0900

    fix: websocket url fix

--- devices/signal.py
+++ devices/signal.py
@@ -7,16 +7,14 @@ import json
  
 async def borrow_signal(place_id):
   # 웹 소켓에 접속을 합니다.
-    async with websockets.connect(f"ws://localhost:8000/ws/devices/{place_id}/",origin="http://127.0.0.1") as websocket:
+    async with websockets.connect(f"ws://asgi:9000/ws/devices/{place_id}/",origin="http://127.0.0.1") as websocket:
     # 10번을 반복하면서 웹 소켓 서버로 메시지를 전송합니다.
         await websocket.send(json.dumps({"message": "borrow"}))
-        time.sleep(1) #to test async flow
         print("borrow success")
 
 
 async def return_signal(place_id):
-    async with websockets.connect(f"ws://localhost:8000/ws/devices/{place_id}/",origin="http://127.0.0.1") as websocket:
+    async with websockets.connect(f"ws://asgi:9000/ws/devices/{place_id}/",origin="http://127.0.0.1") as websocket:
     # 10번을 반복하면서 웹 소켓 서버로 메시지를 전송합니다.
         await websocket.send(json.dumps({"message": "return"}))
-        time.sleep(1)
         print("return success")
